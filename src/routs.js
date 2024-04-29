4f02661c5a3b998d6e78e691ce0eeb05f07970ca
RoberioAlves <roberiouol@gmail.com>
2024-04-29 16:04:22 -0300

    Adicionado novos recursos para  projeto na aula.

--- src/routs.js
+++ src/routs.js
@@ -1,9 +1,17 @@
 import { Router } from 'express'
-
+import { v4 } from 'uuid' 
+import User from './App/models/User'
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-    return response.status(200).json({ message: 'Hello Word!' })
-})
+routes.get('/', async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'Roberio2',
+        emai: 'roberio2@email.com',
+        password_hash: '12345678',
+    });
+    
+    return response.status(201).json(user);
+});
 
 export default routes

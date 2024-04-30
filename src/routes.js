73dbc208090c63d8c31518091e58584cd4b70328
RoberioAlves <roberiouol@gmail.com>
2024-04-30 09:54:00 -0300

    Aualizando projeto add post.

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express'
-import { v4 } from 'uuid' 
-import User from './App/models/User'
+import UserController from './App/controlles/UserController';
+
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Pedro7',
-        email: 'Pedro7@gmail.com',
-        password_hash: '12213443wdasd',
-    });
-
-    return response.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes

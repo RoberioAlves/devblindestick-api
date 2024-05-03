d121fd131462831dc83e45517c2ab4f2ea10e24b
RoberioAlves <roberiouol@gmail.com>
2024-05-03 09:27:30 -0300

    Atualizações do projeto reaizadas e testadas...

--- src/App/controlles/UserController.js
+++ src/App/controlles/UserController.js
@@ -7,21 +7,34 @@ class UserController {
         const schema = Yup.object({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
-            password_hash: Yup.string().min(6).required(),
-            admin: Yup.boolean,
+            password: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        });
+        try{
+        schema.validateSync(request.body, { abortEarly: false});
+    } catch(err) {
+        return response.status(400).json({ error: err.errors})
+    }
+        
+        const { name, email, password, admin } = request.body;
+
+        const userExists = await User.findOne({
+            where: {
+                email,
+            },
         });
 
-        const validation = await schema.isValid(request.body);
-
-        console.log(validation)
-
-        const { name, email, password_hash, admin } = request.body;
+        if(userExists) {
+            return response.status(400).json({
+                error: 'User already exists'
+            })
+        }
 
     const user =await User.create({
         id: v4(),
         name,
         email,
-        password_hash,
+        password,
         admin,
         });
 
